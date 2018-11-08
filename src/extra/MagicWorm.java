fd7499999339198f39fd55a640f53a3e9e6b5dde
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-07 17:00:56 -0800

    Moving worm and checkpoint.

--- src/extra/MagicWorm.java
+++ src/extra/MagicWorm.java
@@ -1,21 +1,36 @@
- int yPosition=0;
-  int xPosition=0;
- 
-void setup(){
-  size(500,500);
   
+
+void setup(){
+  size(495,495);
+  background(0,0,0);
   
 }
 void draw(){
-  background(0,0,0);
-   xPosition++;
-  yPosition++;
+
+  
+makeMagical();
   for(int i=0; i<300; i++){
- stroke(255,255,255);
- fill(255,255,255);
-    ellipse(i*20,i*20,20,20);
+ stroke(random(0, 255), random(0, 255), random(0, 255));
+fill(random(0, 255), random(0, 255), random(0, 255));
+    ellipse(getWormX(30),getWormY(30), 20,20);
     
   }
   
   
+}
+float frequency = .01;
+float noiseInterval = PI;
+
+void makeMagical() {
+     fill( 0, 0, 0, 10 );
+     rect(0, 0, 500, 500);
+     noStroke();
+}
+
+float getWormX(int i) {
+     return map(noise(i*noiseInterval + frameCount * frequency), 0, 1, 0, width);
+}
+
+float getWormY(int i) {
+     return map(noise(i*noiseInterval+1 + frameCount * frequency), 0, 1, 0, height);
 }
